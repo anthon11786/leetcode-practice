154bc940490feed7c2504f6e3c29f2001c818413
anthon11786 <44326656+anthon11786@users.noreply.github.com>
2023-06-25 12:20:44 -0400

    Add howsum problem

---
+++ dp/howsum.py
@@ -0,0 +1,78 @@
+
+# from functools import cache
+
+
+memo = {}
+
+
+def how_sum(target_sum, numbers, memo):
+    """
+
+    """
+    # if memo is None:
+    #     memo = {}
+    # Base cases
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
+    if target_sum in memo:
+        return memo[target_sum]
+
+    # Branching logic
+    for num in numbers:
+        remainder = target_sum - num
+        # same numbers array because can reuse #'s
+        res = how_sum(remainder, numbers, memo)
+        if res is not None:
+            # Early return because can return any valid combo
+            # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            # Python nuance ! use '+' to concatenate lists not .append()
+            memo[target_sum] = res + [num]
+            return memo[target_sum]
+
+    return None
+
+# Time Complexity
+# m = target sum
+# n = numbers.length
+
+# Memoization
+# Time: O()
+# Space: O(m)
+
+
+print(how_sum(7, [2, 3], memo))  # [3, 2, 2]
+print(how_sum(7, [5, 3, 4, 7], memo))  # [4, 3]
+print(how_sum(7, [2, 4], memo))  # null
+print(how_sum(8, [2, 3, 5], memo))  # [2, 2, 2, 2]
+print(how_sum(300, [7, 14], memo))  # null
+print(sum(how_sum(300, [7, 14], memo)))
+
+
+@cache
+def how_sum_cached(target_sum, numbers):
+    """
+
+    """
+    # Base cases
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
+
+    # Branching logic
+    for num in numbers:
+        remainder = target_sum - num
+        # same numbers array because can reuse #'s
+        res = how_sum(remainder, numbers)
+        if res is not None:
+            # Early return because can return any valid combo
+            # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            # Python nuance ! use '+' to concatenate lists not .append()
+            return res + [num]
+
+    return None
+
+
+print(how_sum_cached(300, [7, 14]))  # null
