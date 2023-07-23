9f6500c5aa2f1c388d07d58d1c5b042cd2164d3a
anthon11786 <anthonytperaza@gmail.com>
2023-07-23 17:27:54 -0400

    Adding daily-coding problems

---
+++ daliy-coding-prb/longest_increasing_subsequence.py
@@ -0,0 +1,18 @@
+"""
+Describe an algorithm to compute the longest incresing subsequence 
+of an array of numbers in o(n logn) time
+
+Examples:
+Input: arr[] = {3, 10, 2, 1, 20}
+Output: 3
+Explanation: The longest increasing subsequence is 3, 10, 20
+
+Input: arr[] = {3, 2}
+Output:1
+Explanation: The longest increasing subsequences are {3} and {2}
+""" 
+
+
+
+
+
